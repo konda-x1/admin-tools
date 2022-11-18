6fa38d9ca00895532b51f4d2f5bc5c528a1aad8a
Aleksandar Kondić <konda96@gmail.com>
2022-11-18 15:59:43 +0100

    Check if local port is open before using socat
    This also allows the script to work on older Xen servers where
    the VNC ports to VMs are automatically exposed locally, while
    not having UNIX sockets in /var/run/xen.

--- xen/xen-vm-vnc.sh
+++ xen/xen-vm-vnc.sh
@@ -6,11 +6,12 @@
 #
 # Connects to a Xen server via ssh, lists available VMs and lets you choose which VM you
 # want to connect to via VNC. Locally you must have vncviewer installed, and the remote
-# needs to have socat and xe available. The VNC connection works by first creating an ssh
+# needs to have ss and xe available. If Xen doesn't expose local VNC ports, the remote
+# also needs to have socat available. The VNC connection works by first creating an ssh
 # tunnel and then connecting to 127.0.0.1 with the appropriate port on the remote host --
 # vncviewer takes care of this. And the script ensures the appropriate local port on the
 # remote host is forwarded to point to a corresponding VNC socket file located in
-# /var/run/xen.
+# /var/run/xen if the host doesn't already expose that port locally.
 
 set -e
 
@@ -29,12 +30,14 @@ sshhost="$1"
 
 remotescript=$(cat <<'SSHEOF'
 set -e
-for cmd in xe socat; do
-	if ! command -v "$cmd" &>/dev/null; then
-		echo "remote: Command 'xe' not found" >&2
+require_cmd() {
+	for cmd in "$@"; do if ! command -v "$cmd" &>/dev/null; then
+		echo "remote: Command '$cmd' not found" >&2
 		exit 3
-	fi
-done
+	fi; done
+}
+require_cmd xe ss
+
 uuids=$(xe vm-list power-state=running params=uuid | awk -F'[[:space:]]*:[[:space:]]*' 'NF==2{print $2}')
 doms=$(while IFS= read -u 3 -r i; do
 	gp() { xe vm-param-get uuid="$i" param-name="$1"; }
@@ -44,7 +47,7 @@ options=()
 while IFS= read -u 3 -r domid; do
 	IFS= read -u 3 -r uuid
 	IFS= read -u 3 -r namelabel
-	options+=("dom${domid}: $namelabel ($uuid)")
+	options+=("dom${domid}: $namelabel (uuid: $uuid)")
 done 3<<<"$doms"
 
 echo "Select a VM/domain to make a VNC connection for:" >&2
@@ -61,8 +64,14 @@ if ((item > 1)); then
 else
 	domid=$(head -n 1 <<<"$doms")
 fi
+
+remoteport=$((5900+domid))
+if ! ss -4tan |  awk 'NR>1{print $4}' | awk -F':' '{print $NF}' | grep -q "^$remoteport$"; then
+	require_cmd socat
+	(socat TCP-LISTEN:"$remoteport",fork,bind=127.0.0.1 UNIX-CONNECT:/var/run/xen/vnc-"$domid" &>/dev/null &) || true
+fi
+
 echo "$domid"
-(socat TCP-LISTEN:$((5900+domid)),fork,bind=127.0.0.1 UNIX-CONNECT:/var/run/xen/vnc-"$domid" &>/dev/null &) || true
 SSHEOF
 )
 
