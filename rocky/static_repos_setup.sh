457a37411f75765f7573f284bae96f6c4b570b30
Aleksandar Kondić <konda96@gmail.com>
2024-06-14 22:03:01 +0200

    Add Rocky Linux static repository setup script

---
+++ rocky/static_repos_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Sets up rocky, epel, and powertools repositories, and uses static mirror URLs rather than mirror lists.
+
+ROCKY_MIRROR_BASE='http://dl.rockylinux.org/'
+EPEL_MIRROR_BASE='https://download.example/pub/epel/'
+
+sed "s;^mirrorlist=;#mirrorlist=;; s;^#baseurl=.*dl.rockylinux.org/;baseurl=$ROCKY_MIRROR_BASE;" /etc/yum.repos.d/Rocky-*.repo
+dnf config-manager --set-enabled powertools
+dnf update -y
+dnf install -y epel-release
+crb enable
+sed -i "s;^metalink=;#metalink=;; s;^#baseurl=.*download.example/pub/epel/;baseurl=$EPEL_MIRROR_BASE;" /etc/yum.repos.d/epel*.repo
+dnf update -y
+
